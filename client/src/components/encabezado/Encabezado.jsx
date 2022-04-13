7ac6811dd24f0c8d7e2c910349bc2f2f6b16e779
AlejoBengo <alejobengoechea@yahoo.com.ar>
2022-04-13 16:16:29 -0300

    Nuevo

--- client/src/components/encabezado/Encabezado.jsx
+++ client/src/components/encabezado/Encabezado.jsx
@@ -3,16 +3,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 /*IMPORT DE CSS*/
-import s from "../estilos/searchBar.module.css";
+import s from "../estilos/encabezado.module.css";
 //______________________________________________________________________________
 
 export default function Encabezado() {
   return (
     <div className={s.container}>
-      <h1 className={s.title}>M A P S P H E R E</h1>
+      <h1 className={s.mapsphere}>M A P S P H E R E</h1>
 
-      <Link className={s.link2} to="/home/activity">
-        <button className={s.newAct}>Crear actividad turística</button>
+      <Link to="/home/activity">
+        <button className={s.newActivity}>Crear actividad turística</button>
       </Link>
 
       <Link className={s.link} to="/home">
